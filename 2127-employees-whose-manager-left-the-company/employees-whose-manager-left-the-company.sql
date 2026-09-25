62e4c9669ce2c5942a920210320503b0c2886b48
pinghill-info <ping.hill@theinformationlab.com>
2026-09-25 15:26:40 -0400

    Time: 342 ms (86.02%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -0,0 +1,7 @@
+select 
+E.employee_id
+from Employees E left join Employees M on E.manager_id = M.employee_id
+where E.salary < 30000
+and E.manager_id is not null
+and M.employee_id is null
+order by E.employee_id;
